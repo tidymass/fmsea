b9ec5c9a903bc14d7d7b6574efb734c3176aecf6
jaspershen <shenxt1990@outlook.com>
2022-06-27 13:11:12 -0700

    first release

---
+++ R/1_fmsea_logo.R
@@ -0,0 +1,78 @@
+#' @title Show the base information of fmsea pacakge
+#' @description Show the base information of fmsea pacakge.
+#' @author Xiaotao Shen
+#' \email{shenxt1990@@outlook.com}
+#' @return A ASCII log of fmsea
+#' @importFrom magrittr %>%
+#' @importFrom crayon red yellow green bgRed
+#' @importFrom stringr str_detect str_extract str_extract_all
+#' @importFrom stringr str_replace_all str_replace str_trim str_c str_count
+#' @importFrom readr cols read_csv
+#' @importFrom pbapply pblapply pboptions
+#' @importFrom tibble as_tibble rownames_to_column
+#' @importFrom dplyr mutate filter everything select bind_rows left_join pull
+#' @importFrom plyr dlply .
+#' @importFrom BiocParallel bplapply MulticoreParam SnowParam
+#' @importFrom readxl read_xlsx read_xls
+#' @importFrom purrr map map2
+#' @importFrom ggplot2 aes ggplot geom_point geom_line geom_smooth theme annotate
+#' @importFrom ggplot2 geom_abline theme_bw ggsave geom_segment xlim ylim labs
+#' @importFrom ggplot2 element_line element_text
+#' @importFrom MSnbase readMSData
+#' @importFrom ProtGenerics spectra
+#' @importFrom masstools ms2_match get_os mz_rt_match read_mzxml read_mgf
+#' @importFrom masstools get_spectra_match_score
+#' @importFrom stats lm loess predict
+#' @importFrom plotly ggplotly
+#' @importFrom future plan multisession
+#' @importFrom furrr future_map2 future_map
+#' @importFrom rstudioapi isAvailable hasFun getThemeInfo
+#' @importFrom cli rule symbol cat_line
+#' @importFrom data.table rbindlist
+#' @importFrom progress progress_bar
+#' @importFrom tidyr pivot_longer
+#' @import RColorBrewer
+#' @import utils
+#' @import ggplot2
+#' @import methods
+#' @import graphics
+#' @import grDevices
+#' @import utils
+#' @importClassesFrom massdataset mass_dataset
+#' @export
+#' @examples
+#' fmsea_logo()
+
+fmsea_logo <- function() {
+  cat(crayon::green("Thank you for using fmsea!\n"))
+  message(crayon::green("Version", fmsea_version, "(", update_date, ')\n'))
+  cat(crayon::green("More information: google tidymass fmsea.\n"))
+  cat(crayon::green(
+    c(
+      "   __ __  __  _____ ______          ",
+      "  / _|  \\/  |/ ____|  ____|   /\\    ",
+      " | |_| \\  / | (___ | |__     /  \\   ",
+      " |  _| |\\/| |\\___ \\|  __|   / /\\ \\  ",
+      " | | | |  | |____) | |____ / ____ \\ ",
+      " |_| |_|  |_|_____/|______/_/    \\_\\",
+      "                                    ",
+      "                                    "
+    )
+    
+  ), sep = "\n")
+}
+
+fmsea_version <- utils::packageVersion(pkg = "fmsea")
+update_date <- as.character(Sys.time())
+
+# library(cowsay)
+# # https://onlineasciitools.com/convert-text-to-ascii-art
+# # writeLines(capture.output(say("Hello"), type = "message"), con = "ascii_art.txt")
+# art <- readLines("logo.txt")
+# dput(art)
+# fmsea_logo <-
+#   c("                _    _____  ___ ", " _ __ ___   ___| |_  \\_   \\/   \\",
+#     "| '_ ` _ \\ / _ \\ __|  / /\\/ /\\ /", "| | | | | |  __/ |_/\\/ /_/ /_// ",
+#     "|_| |_| |_|\\___|\\__\\____/___,'  ", "                                "
+#   )
+# cat(fmsea_logo, sep = "\n")
---
+++ R/4_utils.R
@@ -0,0 +1,92 @@
+get_extension = function(file) {
+  tail(stringr::str_split(string = file, pattern = "\\.")[[1]], 1)
+}
+
+readTable = function(file, ...) {
+  extension <- get_extension(file = file)
+  if (extension == "csv") {
+    return(readr::read_csv(file = file, show_col_types = FALSE, ...))
+  }
+  
+  if (extension == 'xlsx') {
+    return(readxl::read_xlsx(path = file, ...))
+  }
+  
+  if (extension == "xls") {
+    return(readxl::read_xls(path = file, ...))
+  }
+  
+  if (extenstion != "csv" &
+      extenstion != "xlsx" &
+      extenstion != "xls") {
+    message(crayon::red("file is not csv, xlsx or xls."))
+  }
+}
+
+
+msg <- function(..., startup = FALSE) {
+  if (startup) {
+    if (!isTRUE(getOption("fmsea.quiet"))) {
+      packageStartupMessage(text_col(...))
+    }
+  } else {
+    message(text_col(...))
+  }
+}
+
+text_col <- function(x) {
+  # If RStudio not available, messages already printed in black
+  if (!rstudioapi::isAvailable()) {
+    return(x)
+  }
+  
+  if (!rstudioapi::hasFun("getThemeInfo")) {
+    return(x)
+  }
+  
+  theme <- rstudioapi::getThemeInfo()
+  
+  if (isTRUE(theme$dark))
+    crayon::white(x)
+  else
+    crayon::black(x)
+  
+}
+
+#' List all packages in the fmsea
+#'
+#' @param include_self Include fmsea in the list?
+#' @export
+#' @return fmsea_packages
+#' @examples
+#' fmsea_packages()
+fmsea_packages <- function(include_self = TRUE) {
+  raw <- utils::packageDescription("fmsea")$Imports
+  imports <- strsplit(raw, ",")[[1]]
+  parsed <- gsub("^\\s+|\\s+$", "", imports)
+  names <-
+    vapply(strsplit(parsed, "\\s+"), "[[", 1, FUN.VALUE = character(1))
+  
+  if (include_self) {
+    names <- c(names, "fmsea")
+  }
+  
+  names
+}
+
+invert <- function(x) {
+  if (length(x) == 0)
+    return()
+  stacked <- utils::stack(x)
+  tapply(as.character(stacked$ind), stacked$values, list)
+}
+
+
+style_grey <- function(level, ...) {
+  crayon::style(paste0(...),
+                crayon::make_style(grDevices::grey(level), grey = TRUE))
+}
+
+
+
+
